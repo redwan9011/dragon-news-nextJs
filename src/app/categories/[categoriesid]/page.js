2bb258271e8cfc216e8d0ef869af8b19ad2aff20
redwan9011 <redwan390309@gmail.com>
2024-07-01 17:00:54 +0600

    Load data category wise

--- src/app/categories/[categoriesid]/page.js
+++ src/app/categories/[categoriesid]/page.js
@@ -1,11 +1,14 @@
+import { getCategoryNews } from '@/Utils/getCategoryNews';
 import React from 'react';
 
-const DynamicNewspage = ({params,searchParams}) => {
-    console.log(searchParams);
+const DynamicNewspage = async({params,searchParams}) => {
+    // console.log(searchParams);
+    const { data} = await getCategoryNews(searchParams.category);
+    // console.log(data);
     return (
         <div>
             {/* dynamic page {params?.categoriesid} <br /> */}
-            dynamic page {searchParams.category}
+            <h1>Total <strong>{searchParams.category}</strong> News: {data.length}</h1>
         </div>
     );
 };
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -22,7 +22,7 @@ const navItems = [
     },
     {
         routes: "Category",
-        path: "/categories/news"
+        path: "/categories/news?category=all-news"
     },
     {
         routes: "Contact",
